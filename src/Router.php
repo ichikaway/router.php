f14bcbd94f45c189cddb8fb846cc4e2683ebab9b
ichikaway <ichikaway@gmail.com>
2026-01-23 17:42:08 +0900

    このsocketから送信したデータはreadされないようにする

--- src/Router.php
+++ src/Router.php
@@ -43,7 +43,10 @@ class Router
                 die("ソケットの作成に失敗しました: " . socket_strerror(socket_last_error()));
             }
             //socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, ['sec' => 5, 'usec' => 0]);
-            //socket_set_option($socket, 263 /*SOL_PACKET*/, 23 /*PACKET_IGNORE_OUTGOING*/, 1);
+
+            // このsocketから送信したデータはreadされないようにする
+            socket_set_option($socket, 263 /*SOL_PACKET*/, 23 /*PACKET_IGNORE_OUTGOING*/, 1);
+
             socket_set_nonblock($socket);
             //socket_set_option($socket, SOL_SOCKET, SO_SNDBUF, 10*1024*1024);
             socket_bind($socket, $nicInfo['device']);
