204402f00ef1231e6bd872466ba94497fa12bf5d
ichikaway <ichikaway@gmail.com>
2026-01-24 16:58:08 +0900

    start.php wlan0 のように引数でeth以外のNIC名を指定できるようにする

--- src/start.php
+++ src/start.php
@@ -5,7 +5,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Dump\Dump;
 use Utils\DeviceInfo;
 
-$nic = (new DeviceInfo())->getDevice();
+// phpのコマンドの引数で、NIC名の指定があればそれを受け取る
+$specifiedNicNameList = array_slice($argv, 1);
+
+$nic = (new DeviceInfo())->getDevice($specifiedNicNameList);
 
 $router = new Router($nic, new Dump(Dump::ALL));
 //$router = new Router($nic, new Dump(Dump::NONE));
