0f8804a15531d280bbd4633923987558ea06f624
ichikaway <ichikaway@gmail.com>
2026-01-25 16:55:59 +0900

    ipv4判定をメソッド対応

--- src/Utils/DeviceInfo.php
+++ src/Utils/DeviceInfo.php
@@ -18,30 +18,36 @@ class DeviceInfo
         foreach ($ifs as $name => $if) {
             if (preg_match('/eth[0-9]/', $name) || in_array($name, $specificNicName, true)) {
                 foreach ($if["unicast"] as $info) {
-                    if (isset($info["address"]) && isset($info["netmask"])) {
-                        if (!filter_var($info["address"], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-                            // IPv4以外のIPアドレスは受け付けない
-                            continue;
-                        }
-                        $nic[] = ['device'  => $name,
-                                  'mac'     => self::macFromIf($name),
-                                  'ip'      => $info["address"],
-                                  'netmask' => $info["netmask"]
+                    if (isset($info["address"]) && isset($info["netmask"]) && $this->isIpv4($info["address"])) {
+                        $nic[] = [
+                            'device'  => $name,
+                            'mac'     => $this->macFromIf($name),
+                            'ip'      => $info["address"],
+                            'netmask' => $info["netmask"]
                         ];
                     }
                 }
             }
         }
-
         if (count($nic) < 2 || is_null($nic[0]['mac'])) {
             throw new \Exception("Device not registered");
         }
         return $nic;
     }
 
-    public static function macFromIf(string $if): ?string
+
+    private function isIpv4(mixed $ipAddress)
+    {
+        if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            return true;
+        }
+        return false;
+    }
+
+    private function macFromIf(string $if): ?string
     {
         $path = "/sys/class/net/{$if}/address";
         return is_readable($path) ? strtolower(trim(file_get_contents($path))) : null;
     }
+
 }
