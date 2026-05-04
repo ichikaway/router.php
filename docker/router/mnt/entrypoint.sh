90b7fc7437e9381350ae517303ae4941bb809b18
ichikaway <ichikaway@gmail.com>
2026-05-04 17:35:07 +0900

    dockerにperfやstraceインストール、phpのスレッドセーフ版を設定、php parallel拡張インストール

--- docker/router/mnt/entrypoint.sh
+++ docker/router/mnt/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 #bash -c "/bin/bash"
+
+#sysctl -w net.bridge.bridge-nf-call-iptables=0
+#sysctl -w net.bridge.bridge-nf-call-ip6tables=0
+#sysctl -w net.bridge.bridge-nf-call-arptables=0
+#
+#nft flush ruleset
+
 tail -f /dev/null
